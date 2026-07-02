66e4fca3795b1bb749a7a5ccc7f822a149aba8b8
Gehna Khanna <gehnakhanna2005@gmail.com>
2026-07-02 09:28:11 +0530

    Time: 27 ms (18.97%), Space: 76.1 MB (88.37%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+
+class Solution {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // Duplicate found within current window
+            if (set.contains(nums[i])) {
+                return true;
+            }
+
+            set.add(nums[i]);
+
+            // Keep window size at most k
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
