e7d725f0eb28158711d9b32b3374568a6bbbf83c
Gehna Khanna <gehnakhanna2005@gmail.com>
2026-07-11 14:05:37 +0530

    Time: 4 ms (99.67%), Space: 47.8 MB (46.66%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,35 +1,29 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        s=s.toLowerCase();
-        int left=0;
-        int right= s.length() - 1;
-        int c=0;
-        while(left<right){
-            if(s.charAt(left)!=s.charAt(right)&&c==0){
-                c++;
-                if (ispalin(s,left+1,right)||ispalin(s,left,right-1)){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            else if(s.charAt(left)!=s.charAt(right)&&c!=0){
-                return false;
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
             }
             left++;
             right--;
         }
+
         return true;
     }
-    private boolean ispalin(String s, int left, int right){
-        while(left<right){
-            if(s.charAt(left)!=s.charAt(right)){
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
                 return false;
-            }
+
             left++;
             right--;
         }
+
         return true;
     }
 }
