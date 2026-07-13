8f9b7d81c9bed813e4a3076b4cf3ff550490380a
Gehna Khanna <gehnakhanna2005@gmail.com>
2026-07-13 16:10:15 +0530

    Time: 17 ms (78.64%), Space: 45.5 MB (48.77%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -3,9 +3,6 @@ class Solution {
         Arrays.sort(nums);
         int ans = nums[0] + nums[1] + nums[2];
         for(int i=0;i<nums.length-2;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
             int left=i+1;
             int right=nums.length-1;
             while(left<right){
